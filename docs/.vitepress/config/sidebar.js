68159e681a20f938bdd3c1bfdd6ba176dda3c8dc
zengyj <zengyj@homeking365.com>
2022-09-07 15:59:50 +0800

    update vitepress

--- docs/.vitepress/config/sidebar.js
+++ docs/.vitepress/config/sidebar.js
@@ -2,10 +2,12 @@ function getFrontendSidebar() {
   return [
     {
       text: '前端',
-      children: [
+      collapsible: true,
+      items: [
         { text: '前端基础', link: '/frontend/basic'},
         { text: '前端架构',
-          children: [
+          collapsible: true,
+          items: [
             { text: '架构概述', link: '/frontend/framework' },
             { text: '前端工程', link: '/frontend/build'},
             { text: 'Monorepo', link: '/frontend/monorepo'}
@@ -13,8 +15,9 @@ function getFrontendSidebar() {
         { text: 'React生态', link: '/frontend/react'},
         { text: 'Vue生态', link: '/frontend/vue'},
         { 
-          text: '前端开发', 
-          children: [
+          text: '前端开发',
+          collapsible: true,
+          items: [
             { text: 'PWA总结', link: '/frontend/pwa'},
             { text: '组件库总结', link: '/frontend/componnet-library' },
             { text: '微前端总结', link: '/frontend/micro-frontend'},
@@ -30,7 +33,8 @@ function getBackendSidebar() {
   return [
     { 
       text: '后端',
-      children: [
+      collapsible: true,
+      items: [
         {text: 'nodejs', link: '/backend/nodejs'},
         {text: 'php', link: '/backend/php'},
         {text: 'go', link: '/backend/go'},
@@ -45,7 +49,8 @@ function getBasicSidebar() {
   return [
     {
       text: '编程基础',
-      children: [
+      collapsible: true,
+      items: [
         {text: '数据结构', link: '/basic/data-structure'},
         {text: '算法', link: '/basic/algorithm'},
         {text: 'http', link: '/basic/http'}
@@ -57,7 +62,8 @@ function getManagerSidebar() {
   return [
     {
       text: '管理',
-      children: [
+      collapsible: true,
+      items: [
         { text: '管理', link: '/manager/basic' },
         { text: '项目管理', link: '/manager/project' },
         { text: '团队管理', link: '/manager/team' }
@@ -69,7 +75,8 @@ function getMeSidebar() {
   return [
     {
       text: '关于我',
-      children: [
+      collapsible: true,
+      items: [
         {text: '个人说明', link: '/me/about'},
         {text: '个人简历', link: '/me/resume'}
       ]
@@ -83,5 +90,4 @@ export default {
   '/basic/': getBasicSidebar(),
   '/manager/': getManagerSidebar(),
   '/me/': getMeSidebar(),
-  '/': []
 }
